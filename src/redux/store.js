ce04a10e14d94410185ad01bbb54f00d7016f8c3
19alexandr80 <usanka1980@gmail.com>
2023-07-19 08:15:46 +0300

    slise

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,29 +1,48 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { createAction, createReducer, createSlice } from '@reduxjs/toolkit';
 
-export const increment = createAction('contacts/increment');
-export const decrement = createAction('contacts/decrement');
+// export const increment = createAction('contacts/increment');
+// export const decrement = createAction('contacts/decrement');
 
 export const incr = createAction('filter/incr');
 
-const contactsReducer = createReducer([], {
-  [increment]: (state, action) => {
-    state.push(action.payload);
-  },
-  [decrement]: (state, action) =>
-    state.filter(({ id }) => {
-      return id !== action.payload;
-    }),
-});
-const filterReducer = createReducer('', {
-  [incr]: (state, action) => {
-    return action;
+// const contactsReducer = createReducer([], builder =>
+//   builder
+//     .addCase(increment, (state, action) => {
+//       state.push(action.payload);
+//     })
+//     .addCase(decrement, (state, action) =>
+//       state.filter(({ id }) => {
+//         return id !== action.payload;
+//       })
+//     )
+// );
+
+const contactsSlise = createSlice({
+  name: 'contacts',
+  initialState: [],
+  reducers: {
+    increment(state, action) {
+      state.push(action.payload);
+    },
+    decrement(state, action) {
+      return state.filter(({ id }) => {
+        return id !== action.payload;
+      });
+    },
   },
 });
 
+const filterReducer = createReducer('', builder =>
+  builder.addCase(incr, (state, action) => {
+    return action;
+  })
+);
+export const { increment, decrement } = contactsSlise.actions;
+
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: contactsSlise.reducer,
     filter: filterReducer,
   },
 });
