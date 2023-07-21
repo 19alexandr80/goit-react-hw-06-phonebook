348c73dc3b0e309c8c43146dbadc9cf3511f8629
19alexandr80 <usanka1980@gmail.com>
2023-07-21 07:25:43 +0300

    memoization ok

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,9 @@
-// import { useSelector } from 'react-redux';
-// import { useMemo } from 'react';
-
 import { FormAddUser } from 'formPhoneBook/FormAddUser';
 import { Filter } from 'contactList/Filter';
 import { ContactList } from 'contactList/ContactList';
 import { AppStyled } from 'AppStyled.styled';
 
 export const App = () => {
-  // const contacts = useSelector(state => state.contacts.value);
-  // const filter = useSelector(state => state.filter);
-
-  // const ren = useMemo(() => {
-  //   if (!filter) {
-  //     return contacts;
-  //   }
-  //   return contacts.filter(({ name }) => {
-  //     return name.toLowerCase().includes(filter.payload.toLowerCase());
-  //   });
-  // }, [filter, contacts]);
   return (
     <AppStyled>
       <h1>Phonebook</h1>
